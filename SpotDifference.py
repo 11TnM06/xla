402a582495c698c6946c2b33c5772d3d3ea1c7f0
Pswrd <hoangducmanh123@gmail.com>
2023-03-30 01:05:54 +0700

    finish SpotDifference

--- SpotDifference.py
+++ SpotDifference.py
@@ -2,41 +2,43 @@ import cv2
 import imutils
 import numpy as np
 
-img1 = cv2.imread("./images/challenge1_1.png")
-img1 = cv2.resize(img1, (550, 360))
-
-img2 = cv2.imread("./images/challenge1_2.png")
-img2 = cv2.resize(img2, (550, 360))
+img1 = cv2.imread("./images/city1.jpg")
+img1 = cv2.resize(img1, (600, 360))
+img2 = cv2.imread("./images/city2.jpg")
+img2 = cv2.resize(img2, (600, 360))
+img_height = img1.shape[0]
 
 # Grayscale
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
 # Find the difference between the two images
 diff = cv2.absdiff(gray1, gray2)
-
-# Show
-#cv2.imshow("diff(img1, img2)", diff)
-# cv2.imshow("Original Picture", img1)
-# cv2.imshow("Modified Picture", img2)
+#cv2.imshow("Difference", diff)
 
 # Threshold the difference image, followed by finding contours to
-threshold = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cv2.imshow("Threshold", threshold)
-# Dilitation
-kernel = np.ones((5,5), np.uint8)
-dilation = cv2.dilate(threshold, kernel, iterations = 1)
-cv2.imshow("Dilation", dilation)
+threshold = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+#cv2.imshow("Threshold", threshold)
+
+# Dilation
+kernel = np.ones((5,5), np.uint8) 
+dilate = cv2.dilate(threshold, kernel, iterations=2) 
+#cv2.imshow("Dilate", dilate)
+
 # Contours
-countours = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+countours = cv2.findContours(dilate.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 countours = imutils.grab_contours(countours)
 for countour in countours:
     if cv2.contourArea(countour) <= 100:
         continue
-    x, y, w, h = cv2.boundingRect(countour)
-    cv2.rectangle(img1, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-x = np.zeros((360, 10, 3), np.uint8)
+    #circle
+    (x, y), radius = cv2.minEnclosingCircle(countour)
+    center = (int(x), int(y))
+    radius = int(radius)
+    cv2.circle(img2, center, radius, (0, 255, 0), 2)
+    
+x = np.zeros((img_height, 10, 3), np.uint8)
 result = np.hstack((img1, x, img2))
 cv2.imshow("Difference", result)
 cv2.waitKey()
--- test.py
+++ test.py
@@ -1,2 +1,32 @@
-def printHello(): 
-    print("Hello")
+import cv2
+import numpy as np
+from skimage.metrics import structural_similarity
+img1 = cv2.imread("./images/city1.jpg")
+img1 = cv2.resize(img1, (600, 360))
+img2 = cv2.imread("./images/city2.jpg")
+img2 = cv2.resize(img2, (600, 360))
+
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+(score, diff) = structural_similarity(gray1, gray2, full=True)
+diff = (diff * 255).astype("uint8")
+
+_, threshold = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+contours = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+
+for contour in contours:
+    if cv2.contourArea(contour) <= 80:
+        continue
+    (x, y, w, h) = cv2.boundingRect(contour)
+    #cv2.rectangle(img1, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+cv2.imshow("Orginal", img1)
+cv2.imshow("Modified", img2)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+
